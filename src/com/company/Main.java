2936447d7bb6f9c150083978e9d4a35f58be8c11
IT-Kubat <kubat88@list.ru>
2019-11-02 23:17:56 +0600

    Lesson1.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world!");
+
+        int myNumber = 54;
+        System.out.println(myNumber);
+
+        myNumber = -9;
+
+        int a;
+
+        a = 7;
+
+        System.out.println(myNumber);
+        System.out.println("результат сложения: " + a + myNumber);
+
+        float var = 15.678f;
+        double var2 = 15.6;
+        char character = '$';
+        boolean isSunny = true;
+        boolean isSmaller = myNumber < a;
+        boolean isEqual = a == myNumber;
+
+        System.out.println(isSunny);
+        System.out.println(!isEqual);
+    }
+}
